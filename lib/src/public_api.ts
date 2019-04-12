d9b77300bda9d0a86222a237a0a2698c492b21ca
Alex Sorafumo <alex@yuion.net>
2019-04-12 22:51:23 +1000

    Fix exporting analytics service to public API

--- lib/src/public_api.ts
+++ lib/src/public_api.ts
@@ -2,7 +2,7 @@
     // Export Library Module
 export * from './library.module';
     // Export Library Services
-
+export * from './services/google-analytics.service';
     // Export Library Directives
 
     // Export Library Classes
--- lib/src/services/google-analytics.service.ts
+++ lib/src/services/google-analytics.service.ts
@@ -33,10 +33,7 @@ export class GoogleAnalyticsService {
             i['GoogleAnalyticsObject'] = r;
             (i[r] =
                 i[r] ||
-                function() {
-                    (i[r].q = i[r].q || []).push(arguments);
-                }),
-                (i[r].l = 1 * new Date());
+                function() { (i[r].q = i[r].q || []).push(arguments); }), (i[r].l = 1 * (new Date() as any));
             (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
             a.async = 1;
             a.src = g;
